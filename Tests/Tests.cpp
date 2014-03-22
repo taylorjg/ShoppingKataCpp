f8ab906b285e5f08772d3393bfdc20e2ad95e82e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-03-22 20:00:55 +0000

    Tweaks

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -6,43 +6,40 @@ using namespace ShoppingKataCode;
 
 #include <UnitTest++.h>
 
-SUITE(ShoppingKataTests)
+class ShoppingKataTests
 {
-	class ShoppingKataTestsFixture
+protected:
+	void AddItemsToBasket(const std::string& items, Basket& basket)
 	{
-	protected:
-		void AddItemsToBasket(const std::string& items, Basket& basket)
-		{
-			for (auto c : items) {
-				auto item = std::string(1, c); 
-				basket.Add(item);
-			}
+		for (auto c : items) {
+			auto item = std::string(1, c); 
+			basket.Add(item);
 		}
+	}
 
-		void CheckCostAfterAddingItems(const std::string& items, int expectedCost)
-		{
-			AddItemsToBasket(items, _basket);
-			auto actualCost = _checkout.Process(_basket);
-			CHECK_EQUAL(expectedCost, actualCost);
-		}
+	void CheckCostAfterAddingItems(const std::string& items, int expectedCost)
+	{
+		AddItemsToBasket(items, _basket);
+		auto actualCost = _checkout.Process(_basket);
+		CHECK_EQUAL(expectedCost, actualCost);
+	}
 
-		Checkout _checkout;
-		Basket _basket;
-	};
+	Checkout _checkout;
+	Basket _basket;
+};
 
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithNothing) { CheckCostAfterAddingItems("", 0); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithA) { CheckCostAfterAddingItems("A", 50); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithB) { CheckCostAfterAddingItems("B", 30); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithC) { CheckCostAfterAddingItems("C", 20); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithD) { CheckCostAfterAddingItems("D", 15); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithAB) { CheckCostAfterAddingItems("AB", 80); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithAAA) { CheckCostAfterAddingItems("AAA", 50 + 50 + 50 - 20); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithBB) { CheckCostAfterAddingItems("BB", 30 + 30 - 15); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithBBBB) { CheckCostAfterAddingItems("BBBB", (30 + 30 - 15) + (30 + 30 - 15)); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithAAABBBBC) { CheckCostAfterAddingItems("AAABBBBC", (50 + 50 + 50 - 20) + (30 + 30 - 15) + (30 + 30 - 15) + 20); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithBABAABBC) { CheckCostAfterAddingItems("BABAABBC", (50 + 50 + 50 - 20) + (30 + 30 - 15) + (30 + 30 - 15) + 20); }
-	TEST_FIXTURE(ShoppingKataTestsFixture, BasketWithBBB) { CheckCostAfterAddingItems("BBB", (30 + 30 - 15) + 30); }
-}
+TEST_FIXTURE(ShoppingKataTests, EmptyBasket) { CheckCostAfterAddingItems("", 0); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithA) { CheckCostAfterAddingItems("A", 50); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithB) { CheckCostAfterAddingItems("B", 30); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithC) { CheckCostAfterAddingItems("C", 20); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithD) { CheckCostAfterAddingItems("D", 15); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithAB) { CheckCostAfterAddingItems("AB", 80); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithAAA) { CheckCostAfterAddingItems("AAA", 130); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithBB) { CheckCostAfterAddingItems("BB", 45); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithBBBB) { CheckCostAfterAddingItems("BBBB", 90); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithAAABBBBC) { CheckCostAfterAddingItems("AAABBBBC", 130 + 45 + 45 + 20); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithBABAABBC) { CheckCostAfterAddingItems("BABAABBC", 130 + 45 + 45 + 20); }
+TEST_FIXTURE(ShoppingKataTests, BasketWithBBB) { CheckCostAfterAddingItems("BBB", 45 + 30); }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
